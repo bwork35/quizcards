f33c6dabfae1a8e97c92c7f638ae70c87e2f1fdc
Bryan Workman <bryanworkman@macbookpro.lan>
2025-05-29 14:14:29 -0700

    adds Models

---
+++ quizcards/quizcards/Models/Collection.swift
@@ -0,0 +1,50 @@
+import Foundation
+import CloudKit
+
+struct FlashStrings {
+    static let recordTypeKey = "Collection"
+    fileprivate static let subjectKey = "subject"
+    fileprivate static let flashcardKey = "flashcards"
+    fileprivate static let timestampKey = "timestamp"
+}
+
+class Collection {
+    var subject: String
+    var flashcards: [Flashcard]
+    var timestamp: Date
+    
+    let recordID: CKRecord.ID
+    
+    init (subject: String, flashcards: [Flashcard] = [], timestamp: Date = Date(), recordID: CKRecord.ID = CKRecord.ID(recordName: UUID().uuidString)) {
+        self.subject = subject
+        self.flashcards = flashcards
+        self.timestamp = timestamp
+        self.recordID = recordID
+    }
+}
+
+extension Collection {
+    convenience init?(ckRecord: CKRecord) {
+        guard let subject = ckRecord[FlashStrings.subjectKey] as? String,
+            let timestamp = ckRecord[FlashStrings.timestampKey] as? Date else {return nil}
+        
+        self.init(subject: subject, flashcards: [], timestamp: timestamp, recordID: ckRecord.recordID)
+    }
+}
+
+extension CKRecord {
+    convenience init(collection: Collection) {
+        self.init(recordType: FlashStrings.recordTypeKey, recordID: collection.recordID)
+        
+        self.setValuesForKeys([
+            FlashStrings.subjectKey : collection.subject,
+            FlashStrings.timestampKey : collection.timestamp
+        ])
+    }
+}
+
+extension Collection: Equatable {
+    static func == (lhs: Collection, rhs: Collection) -> Bool {
+        return lhs.recordID == rhs.recordID
+    }
+} 
